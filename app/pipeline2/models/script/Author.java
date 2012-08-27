cf7aefecea20f016db4da8b9e3b043af2504dd28
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2012-08-27 12:09:45 +0200

    Split the pipeline2 package into its own repository: https://github.com/daisy-consortium/pipeline-clientlib-java

--- app/pipeline2/models/script/Author.java
+++
@@ -1,9 +0,0 @@
-package pipeline2.models.script;
-
-/** Information about the scripts author. */
-public class Author {
-	public String name;
-	public String contact;
-	public String organization;
-	public Homepage homepage;
-}
