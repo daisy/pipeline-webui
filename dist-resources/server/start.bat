d0ba828329fa7c7ef54b915208cc457b91246372
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2012-12-13 16:54:59 +0100

    updated server start scripts

--- dist-resources/server/start.bat
+++ dist-resources/server/start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
+echo Starting Web UI...
 cd /d "%~dp0"
-java -Dconfig.file="%~dp0/application.conf" %* -cp "%~dp0\lib\*;" play.core.server.NettyServer %~dp0
-
+java -Dconfig.file="%~dp0/application.conf" %* -cp "%~dp0\lib\*;" play.core.server.NettyServer .
