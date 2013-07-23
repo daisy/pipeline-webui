39f93908198de636fab85d7e1612e5f14e855292
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2013-07-23 16:44:15 +0200

    updated log4j versions to fix java.lang.NoSuchFieldError: TRACE

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,8 @@ object ApplicationBuild extends Build {
       "org.daisy.pipeline" % "clientlib-java" % "1.0-SNAPSHOT",
       "org.apache.commons" % "commons-compress" % "1.4.1",
       "org.apache.commons" % "commons-email" % "1.2",
-      "log4j" % "apache-log4j-extras" % "1.0"
+      "log4j" % "log4j" % "1.2.17",
+      "log4j" % "apache-log4j-extras" % "1.1"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
